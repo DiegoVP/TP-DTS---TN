068a933f49e36d618f569e5dad96a36a3c966f0b
Sebastían Burgat <sebaburgat@gmail.com>
2019-10-21 21:04:36 -0300

    se corrige"Mal uso de $_POST en funciones.php"

--- funciones.php
+++ funciones.php
@@ -1,98 +1,97 @@
 <?php
 function validarRegistro($datos,$imagen){
-  $_FILES=$imagen;
-  $_POST=$datos;
+
   $contrasenia = "";
   $avatarAdress="";
   $errores = false;
 
   //NOMBRE
-    if($_POST["nombre"] == "")  {
-        $_POST["nombre"] = "Complete el campo con su nombre.";
+    if($datos["nombre"] == "")  {
+        $datos["nombre"] = "Complete el campo con su nombre.";
         $errores = true;
     }
-    else if(strlen($_POST["nombre"]) < 2){
-        $_POST["nombre"] = "Su nombre debe tener al menos 2 caracteres";
+    else if(strlen($datos["nombre"]) < 2){
+        $datos["nombre"] = "Su nombre debe tener al menos 2 caracteres";
         $errores = true;
     }
 
   //APELLIDO
-    if($_POST["apellido"] == "")  {
-        $_POST["apellido"] = "Complete el campo con su apellido.";
+    if($datos["apellido"] == "")  {
+        $datos["apellido"] = "Complete el campo con su apellido.";
         $errores = true;
     }
 
   //EMAIL
-    if($_POST["email"] == "")  {
-        $_POST["email"] = "Complete su direccion de e-mail";
+    if($datos["email"] == "")  {
+        $datos["email"] = "Complete su direccion de e-mail";
         $errores = true;
     }
-    else if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
-        $_POST["email"] = "Su direccion de correo no es valida";
+    else if(!filter_var($datos["email"], FILTER_VALIDATE_EMAIL)){
+        $datos["email"] = "Su direccion de correo no es valida";
         $errores = true;
     }
 
   //USERNAME
-    if($_POST["username"] == "")  {
-        $_POST["username"] = "Elija su nombre de Usuario.";
+    if($datos["username"] == "")  {
+        $datos["username"] = "Elija su nombre de Usuario.";
         $errores = true;
     }
-    else if(strlen($_POST["username"]) < 2)  {
-        $_POST["username"] = "Su nombre de usuario debe ser más extenso.";
+    else if(strlen($datos["username"]) < 2)  {
+        $datos["username"] = "Su nombre de usuario debe ser más extenso.";
         $errores = true;
     }
 
   //AVATAR
-    if(isset($_FILES["avatar"])){
-        $tipoImagen = $_FILES['avatar']['type'];
+    if(isset($imagen["avatar"])){
+        $tipoImagen = $imagen['avatar']['type'];
         $avatarAdress = "";
         if( $tipoImagen == 'image/png' ||
             $tipoImagen == 'image/jpg' ||
             $tipoImagen == 'image/jpeg' ||
             $tipoImagen == 'image/gif'){
-            $ext = pathinfo($_FILES['avatar']['name'],  PATHINFO_EXTENSION);
-            $avatarAdress = 'fotos/' . $_POST['email'] . '.' . $ext;
+            $ext = pathinfo($imagen['avatar']['name'],  PATHINFO_EXTENSION);
+            $avatarAdress = 'fotos/' . $datos['email'] . '.' . $ext;
         }else{
-            $_POST[] = ["validacion_imagen" => "El formato debe ser png, jpg o gif"];
+            $datos[] = ["validacion_imagen" => "El formato debe ser png, jpg o gif"];
             $errores = true;
         }
     }
 
 
   //PASSWORD
-    if(strlen($_POST["password"]) < 6)  {
-        $_POST["password"] = "La clave debe tener al menos 6 caracteres";
+    if(strlen($datos["password"]) < 6)  {
+        $datos["password"] = "La clave debe tener al menos 6 caracteres";
         $errores =true;
     }
-    if(strlen($_POST["password"]) > 16)  {
-        $_POST["password"] = "La clave no puede tener más de 16 caracteres";
+    if(strlen($datos["password"]) > 16)  {
+        $datos["password"] = "La clave no puede tener más de 16 caracteres";
         $errores =true;
     }
-    if (!preg_match('`[a-z]`',$_POST["password"]))  {
-        $_POST["password"] = "La clave debe tener al menos una letra minúscula";
+    if (!preg_match('`[a-z]`',$datos["password"]))  {
+        $datos["password"] = "La clave debe tener al menos una letra minúscula";
         $errores =true;
     }
-    if (!preg_match('`[A-Z]`',$_POST["password"]))  {
-        $_POST["password"] = "La clave debe tener al menos una letra mayúscula";
+    if (!preg_match('`[A-Z]`',$datos["password"]))  {
+        $datos["password"] = "La clave debe tener al menos una letra mayúscula";
         $errores =true;
     }
-    if (!preg_match('`[0-9]`',$_POST["password"]))  {
-        $_POST["password"] = "La clave debe tener al menos un caracter numérico";
+    if (!preg_match('`[0-9]`',$datos["password"]))  {
+        $datos["password"] = "La clave debe tener al menos un caracter numérico";
         $errores =true;
     }
-    if($_POST["password"] != $_POST["passwordConfirm"])  {
-        $_POST["password"] = "Las contraseñas deben coincidir";
-        $_POST["passwordConfirm"] = "Las contraseñas deben coincidir";
+    if($datos["password"] != $datos["passwordConfirm"])  {
+        $datos["password"] = "Las contraseñas deben coincidir";
+        $datos["passwordConfirm"] = "Las contraseñas deben coincidir";
         $errores = true;
     }
     else  {
-        $contrasenia = password_hash($_POST["password"],PASSWORD_DEFAULT);
+        $contrasenia = password_hash($datos["password"],PASSWORD_DEFAULT);
     }
 
   //RECORDARME
-    if(isset($_POST["recordarme"])){
-      if($_POST["recordarme"] != null){
-      $auxusername=$_POST["username"];
+    if(isset($datos["recordarme"])){
+      if($datos["recordarme"] != null){
+      $auxusername=$datos["username"];
         setcookie("username","$auxusername", time() + 60 * 60* 24 );
       }
     }
@@ -100,10 +99,10 @@ function validarRegistro($datos,$imagen){
     if(!$errores)  {
 
         $usuario = [
-          "nombre" => $_POST["nombre"],
-          "apellido" => $_POST["apellido"],
-          "email" => $_POST["email"],
-          "username" => $_POST["username"],
+          "nombre" => $datos["nombre"],
+          "apellido" => $datos["apellido"],
+          "email" => $datos["email"],
+          "username" => $datos["username"],
           "password" => $contrasenia,
           "avatarAdress" => $avatarAdress,
           "activo" => 1
@@ -116,25 +115,24 @@ function validarRegistro($datos,$imagen){
         file_put_contents("usuarios.json",$nuevosUsuariosEnJSON);
 
   // Guardo de imagen
-        move_uploaded_file($_FILES['avatar']['tmp_name'], $avatarAdress);
+        move_uploaded_file($imagen['avatar']['tmp_name'], $avatarAdress);
 
 
   //INICIA SESSION
-        $_SESSION["username"] = $_POST["username"];
+        $_SESSION["username"] = $datos["username"];
         header("Location:index.php");
       exit;
     }
-  return $_POST;
+  return $datos;
 }
 
 function validarLogin($datos){
-  $_POST=$datos;
   $usuariosEnJSON = file_get_contents("usuarios.json");
   $usuarios = json_decode($usuariosEnJSON,true);
   foreach($usuarios as $usuario){
-    if($usuario["email"] == $_POST["email"]){
-        if(password_verify($_POST["password"],$usuario["password"])){
-            if($_POST["recordarme"] != null){
+    if($usuario["email"] == $datos["email"]){
+        if(password_verify($datos["password"],$usuario["password"])){
+            if($datos["recordarme"] != null){
                 $username=$usuario["username"];
                 setcookie("username", "$username", time() + 60 * 60* 24 );
                 var_dump($_COOKIE);
